368d905d8f199a870380bdf1f2b3ffe442d1c9a6
duyenqa <ttemp8399@gmail.com>
2025-05-18 17:19:06 +0700

    update code

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,28 +1,44 @@
-import {NextIntlClientProvider, hasLocale} from 'next-intl';
-import {notFound} from 'next/navigation';
-import {routing} from '@/i18n/routing';
-import Header from "@/components/Header";
-import Footer from '@/components/Footer';
- 
+import { NextIntlClientProvider, hasLocale } from 'next-intl';
+import { notFound } from 'next/navigation';
+import { routing } from '@/i18n/routing';
+import Link from 'next/link';
+import DropdownMenu from '@/components/DropdownMenu';
+import "./globals.css";
+
 export default async function LocaleLayout({
   children,
   params
 }: {
   children: React.ReactNode;
-  params: Promise<{locale: string}>;
+  params: Promise<{ locale: string }>;
 }) {
   // Ensure that the incoming `locale` is valid
-  const {locale} = await params;
+  const { locale } = await params;
   if (!hasLocale(routing.locales, locale)) {
     notFound();
   }
- 
+
   return (
     <html lang={locale}>
       <body>
-        <Header/>
-        <NextIntlClientProvider>{children}</NextIntlClientProvider>
-        <Footer/>
+        <div className="container">
+          <div className="wrapper">
+            <div className="navbar">
+              <div className="main-menu">
+                <Link href="/">Home</Link>
+                <DropdownMenu />
+              </div>
+            </div>
+            <NextIntlClientProvider>
+              {children}
+            </NextIntlClientProvider>
+            <footer>
+              <p>
+                &copy; Copyright by Ngô Thị Kim Duyên 2025 - {new Date().getFullYear()}
+              </p>
+            </footer>
+          </div>
+        </div>
       </body>
     </html>
   );
---
+++ src/components/DropdownMenu.tsx
@@ -0,0 +1,29 @@
+"use client"
+import { ChangeEvent, useTransition } from "react";
+import { useRouter } from "next/navigation";
+function DropdownMenu() {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+
+  const onSelectLanguages = (e: ChangeEvent<HTMLSelectElement>) => {
+    const valueLang = e.target.value;
+    
+    startTransition(() => {
+      router.replace(`/${valueLang}`);
+    })
+  }
+
+ return (
+   <>
+    <label>
+      <span className="text">Choose languages:</span>
+      <select onChange={onSelectLanguages}>
+      <option value="en">English</option>
+      <option value="vi">Việt Nam</option>
+    </select>
+    </label>
+   </>
+ )
+};
+
+export default DropdownMenu;
