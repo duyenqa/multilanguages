596e6c63836f6d016ac9534052f1d56341fd1281
duyenqa <ttemp8399@gmail.com>
2025-05-19 11:37:15 +0700

    fix bug dropdown menu language

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,32 +1,42 @@
 "use client"
 
-import { ChangeEvent, useTransition } from "react";
+import { ChangeEvent, useEffect, useState, useTransition } from "react";
 import { usePathname, useRouter } from "next/navigation";
 
 export default function DropdownMenu() {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
+  const [newLang, setNewLang] = useState<string>('');
   const pathname = usePathname();
 
   const onSelectLanguages = (e: ChangeEvent<HTMLSelectElement>) => {
     const valueLang = e.target.value;
-
-    // Kiểm tra xem ngôn ngữ hiện tại có khác với ngôn ngữ chọn không
+    setNewLang(valueLang); 
+    
     if (pathname !== `/${valueLang}`) {
-      console.log(`Changing language to: ${valueLang}`);
-
       startTransition(() => {
         router.replace(`/${valueLang}`);
       });
     }
   };
 
+  useEffect(() => {
+    const savedLang = localStorage.getItem('lang');
+    if (savedLang) {
+      setNewLang(savedLang);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('lang', newLang);
+  }, [newLang]);
+
   return (
     <>
       <label>
         <span className="text">Choose languages:</span>
         <select onChange={onSelectLanguages}>
-          <option value="none">Select:</option>
+          <option value="none">Select: {newLang}</option>
           <option value="en">English</option>
           <option value="vi">Vietnamese</option>
           <option value="japan">Japanese</option>
