41f8550ff259e404db0fdb0433da96c4467b2fbd
duyenqa <ttemp8399@gmail.com>
2025-05-19 11:11:48 +0700

    fix bug translate englishenglish

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,25 +1,32 @@
 "use client"
 
 import { ChangeEvent, useTransition } from "react";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function DropdownMenu() {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
+  const pathname = usePathname();
 
   const onSelectLanguages = (e: ChangeEvent<HTMLSelectElement>) => {
     const valueLang = e.target.value;
 
-    startTransition(() => {
-      router.replace(`/${valueLang}`);
-    })
-  }
+    // Kiểm tra xem ngôn ngữ hiện tại có khác với ngôn ngữ chọn không
+    if (pathname !== `/${valueLang}`) {
+      console.log(`Changing language to: ${valueLang}`);
+
+      startTransition(() => {
+        router.replace(`/${valueLang}`);
+      });
+    }
+  };
 
   return (
     <>
       <label>
         <span className="text">Choose languages:</span>
         <select onChange={onSelectLanguages}>
+          <option value="none">Select:</option>
           <option value="en">English</option>
           <option value="vi">Vietnamese</option>
           <option value="japan">Japanese</option>
